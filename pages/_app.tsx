ac3699e449a2cad0f2f59eb8830db4da84e74ec1
yassinebk <yassinebk23@gmail.com>
2022-01-16 11:48:14 +0100

    initial commit

---
+++ pages/_app.tsx
@@ -0,0 +1,13 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import "../styles/globals.css";
+import theme from "../styles/theme";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
+}
+
+export default MyApp;
---
+++ styles/theme.ts
@@ -0,0 +1,18 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  colors: {
+    darkBackground: "#16222A",
+    secondaryLight: "#143258",
+    cardDark: "#25313A",
+    cardDark2: "#293640",
+    darkSecondary: "#CFE4FF",
+    lightBackground: "#ECEEF1",
+    },
+    
+});
+
+
+
+
+export default theme;
