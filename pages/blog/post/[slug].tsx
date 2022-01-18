1d9035c7028b4819047843b24bc50c6e0628dbb7
yassinebk <yassinebk23@gmail.com>
2022-01-18 06:48:55 +0100

    added funcs and markdown

---
+++ pages/blog/post/[slug].tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../../../components/Layout';
+import ReactMarkdown from 'react-markdown'
+
+
+interface SlugProps {
+
+}
+
+export const Slug: React.FC<SlugProps> = ({}) => {
+        return (<Layout title={''}></Layout>);
+}
