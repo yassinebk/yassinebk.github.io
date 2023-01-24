c4af26f9bd3e7e8eb8969cb0b7f04970e596c97c
fir3cr4ckers <62627838+yassinebk@users.noreply.github.com>
2023-01-24 15:19:45 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,18 +1,4 @@
-const isGithubActions = process.env.GITHUB_ACTIONS || false
-
-let assetPrefix = ''
-let basePath = '/'
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
-}
-
 module.exports = {
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
   reactStrictMode: true,
   images: {
     domains: ['res.cloudinary.com', 'scontent.ftun6-1.fna.fbcdn.net'],
